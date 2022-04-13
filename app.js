b4c2f0f36d7156faa533680cd5c8ec6ec89a55c4
MacUer <macuer@MacUers-MacBook-Pro.local>
2022-04-13 17:31:10 -0400

    some message

---
+++ app.js
@@ -0,0 +1,21 @@
+var a = 10;
+var b = 20;
+var c = 30;
+var d = 40;
+var e = 50;
+var f = 60;
+var g = 70;
+var h = 80;
+var i = 90;
+var j = 100;
+
+console.log(a+b)
+console.log(b+c)
+console.log(f-e)
+console.log(h-g)
+console.log(e*a)
+console.log(f*e)
+console.log(h/b)
+console.log(f/a)
+console.log(a++)
+console.log(b--)
